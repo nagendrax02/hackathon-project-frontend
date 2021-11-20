fdef0bb9a0e974588093f9b6424902ae758eb6af
prjal-Bankoti <prajalbankoti971@gmail.com>
2021-11-21 00:49:42 +0530

    >

--- hackathon-project/src/components/customer-card/CustomerCard.jsx
+++ hackathon-project/src/components/customer-card/CustomerCard.jsx
@@ -1,45 +1,116 @@
-import {Card,Box,Button} from "@mui/material"
-import {BsCircleFill} from "react-icons/bs"
-import {BsFillSquareFill} from "react-icons/bs"
-import {VscCircleLargeFilled} from "react-icons/vsc"
-import {FaUserAlt} from "react-icons/fa"
-export const CustomerCard = ()=>{
-    return <> 
-    <Box sx={{padding:'50px', display:'flex', flex:'50%', flexWrap:'wrap'}}>
-
-
-        <Card sx={{ width:'35%', margin:'auto', marginBottom:'30px',}}>
-            <Box sx={{ background:'rgb(238,245,254)', padding:'20px'}}>
-
-            <Box sx={{fontWeight:'600', marginBottom:'10px'}}>Name</Box>
-            <Box sx={{fontSize:'12px'}}><BsCircleFill size='8'></BsCircleFill>&nbsp; Source Location</Box>
-            <Box sx={{borderLeft:'2px solid black', height:'20px', marginLeft:'3px', marginBottom:'2px',marginTop:'2px'}}>     <Box sx={{display:'flex', justifyContent:'center', marginLeft:'100px',fontSize:'20px' }}>₹ &nbsp;22000 <Box sx={{fontSize:'10px', padding:'10px'}}> &nbsp;Per Tonne</Box></Box> </Box>
-            <Box sx={{display:'flex', fontSize:'12px'}}><BsFillSquareFill style={{marginTop:'4px'}} size="8"/>&nbsp; Destination Location 
-             
+import { Card, Box, Button } from "@mui/material";
+import { BsCircleFill } from "react-icons/bs";
+import { BsFillSquareFill } from "react-icons/bs";
+import { VscCircleLargeFilled } from "react-icons/vsc";
+import { FaUserAlt } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 
 
-            </Box>
-         <Box sx={{display:'flex', gap:'10px', padding:'15px'}}>
-        <Box sx={{fontSize:'12px', padding:'5px'}}>4 Tonne(s) Truck</Box>
-       <Box> <VscCircleLargeFilled size='8' color='gray'/> </Box>
-        <Box sx={{fontSize:'12px',padding:'5px'}}>To be Billed</Box>
+import CircularProgress from "@mui/material/CircularProgress";
+
+export const CustomerCard = () => {
+  const [data, setData] = useState();
+  async function getData() {
+    try {
+      const ref = await fetch("http://localhost:2222/user/business_customer");
+      const data = await ref.json();
+      console.log(data);
+      setData(data);
+    } catch (err) {
+      console.log(err.massage);
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+  if (!data) {
+    return (
+      <>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            margin: "auto",
+            marginTop: "100px",
+          }}
+        >
+          <CircularProgress />
         </Box>
-        </Box>
+      </>
+    );
+  }
+  return (
+    <>
+      <Box
+        sx={{ padding: "50px", display: "flex", flex: "50%", flexWrap: "wrap" }}
+      >
+        {data.map((e) => {
+          return (
+            <Card sx={{ width: "43%", margin: "auto", marginBottom: "30px" }}>
+              <Box sx={{ background: "rgb(238,245,254)", padding: "20px" }}>
+                <Box sx={{ fontWeight: "600", marginBottom: "10px" }}>{e.material}</Box>
+                <Box sx={{ fontSize: "12px" }}>
+                  <BsCircleFill size="8"></BsCircleFill>&nbsp; {e.route1}
+                </Box>
+                <Box
+                  sx={{
+                    borderLeft: "2px solid black",
+                    height: "20px",
+                    marginLeft: "3px",
+                    marginBottom: "2px",
+                    marginTop: "2px",
+                  }}
+                >
+                  {" "}
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      marginLeft: "100px",
+                      fontSize: "20px",
+                    }}
+                  >
+                    ₹ &nbsp;{e.price}
+                    <Box sx={{ fontSize: "10px", padding: "10px" }}>
+                      {" "}
+                      &nbsp;Per Tonne
+                    </Box>
+                  </Box>{" "}
+                </Box>
+                <Box sx={{ display: "flex", fontSize: "12px" }}>
+                  <BsFillSquareFill style={{ marginTop: "4px" }} size="8" />
+                  &nbsp;  {e.route2}
+                </Box>
+                <Box sx={{ display: "flex", gap: "10px", padding: "15px" }}>
+                  <Box sx={{ fontSize: "12px", padding: "5px" }}>
+                    4 Tonne(s) Truck
+                  </Box>
+                  <Box>
+                    {" "}
+                    <VscCircleLargeFilled size="8" color="gray" />{" "}
+                  </Box>
+                  <Box sx={{ fontSize: "12px", padding: "5px" }}>
+                    To be Billed
+                  </Box>
+                </Box>
+              </Box>
 
-        <Box sx={{padding:'20px', display:'flex', gap:'100px'}}>
-           <Box sx={{display:'flex', gap:'20px', padding:'10px'}}> <FaUserAlt></FaUserAlt> 
-            <Box>Sanman Traders</Box>
-            </Box>
-           <Button variant='contained' sx={{width:'100px'}}>Bid</Button>
-        </Box>
-       
-        </Card>
+              <Box sx={{ padding: "20px", display: "flex", gap: "100px" }}>
+                <Box sx={{ display: "flex", gap: "20px", padding: "10px" }}>
+                  {" "}
+                  <FaUserAlt></FaUserAlt>
+                  <Box>Sanman Traders</Box>
+                </Box>
+                <Button variant="contained" sx={{ width: "100px" }}>
+                  Bid
+                </Button>
+              </Box>
+            </Card>
+          );
+        })}
 
-
-        <Card sx={{padding:'20px', width:'35%', margin:'auto', marginBottom:'30px', background:'rgb(238,245,254)'}}>Customer Card</Card>
-    
-
-        </Box>
+      </Box>
     </>
-}
+  );
+};
